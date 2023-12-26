f46b38fd585a7d04854fc17d6c3ac4b5bf94ebd4
Sebastian Agustin Maza <sebas030.maza@gmail.com>
2023-12-26 19:08:24 -0300

    se agrego react bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Container } from 'react-bootstrap'
 
 function App() {
   return (
-    <>
-    </>
+    <Container className='my-5'>
+      <h1 className='text-center'>Hola mundo</h1>
+    </Container>
   )
 }
 
